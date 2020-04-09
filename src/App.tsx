42eed2a6a93b0185e595571f1992defd837fb822
wenhan <wenhan.work@gmail.com>
2020-04-09 22:27:37 +0800

    add Menu and MenuItem components.

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,8 @@ import React from "react";
 import logo from "./logo.svg";
 import "./styles/index.scss";
 import Button, { ButtonType, ButtonSize } from "./components/Button/button";
+import Menu from "./components/Menu/menu";
+import MenuItem from "./components/Menu/menuItem";
 
 function App() {
   return (
@@ -14,6 +16,11 @@ function App() {
       <Button btnType={ButtonType.Default} autoFocus>
         Default
       </Button>
+      <Menu defaultIndex={0} onSelect={(index) => alert(index)}>
+        <MenuItem index={0}>Cool Link1</MenuItem>
+        <MenuItem index={1}>Cool Link2</MenuItem>
+        <MenuItem index={2}>Cool Link3</MenuItem>
+      </Menu>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
